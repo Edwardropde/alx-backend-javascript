8071bd4e0002fde3d4f9589c9cda3f5d24012046
Edwardropde <134831265+Edwardropde@users.noreply.github.com>
2024-04-30 16:33:56 +0300

    6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,21 +1,14 @@
-// Import the Express module
 const express = require('express');
 
-// Create an Express application
 const app = express();
-
-// Define a route handler for the root path
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
-});
-
-// Set the port to listen on
 const PORT = 1245;
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-// Export the app variable
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
 module.exports = app;
